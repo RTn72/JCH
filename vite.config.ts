a8bf057ad9e25b312435abd21748d3c0c0b9fbf5
Ratan <prajapatratan880@gmail.com>
2026-09-18 16:08:53 +0530

    Fix Vercel deployment with Nitro

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
   server: {
     port: 5173,
   },
-  plugins: [tsConfigPaths(), tanstackStart(), viteReact(), tailwindcss()],
+  plugins: [tsConfigPaths(), tanstackStart(), nitro(), viteReact(), tailwindcss()],
 });
